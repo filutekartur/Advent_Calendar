f1ef15bd7c6254f372b2a059893f320937488f4a
artur <filutekartur@interia.pl>
2025-10-08 20:47:06 +0200

    day model created with few fields

--- advent_calendar/models.py
+++ advent_calendar/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class day(models.Model):
+    days = range(1,25)
+    day = models.SmallIntegerField(blank=False,max_length=2,choices=days,unique=True)
+    is_unlocked = models.BooleanField(blank=False)
+    award_quote = models.CharField(blank=True)
+    award_image = models.CharField(blank=True)
+    award_link = models.URLField(max_length=255,blank=True)
+    def __str__(self):
+        return self.day
