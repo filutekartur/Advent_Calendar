7369e11e4f4d838cb77adc837294199a5018f6d7
artur <filutekartur@interia.pl>
2025-10-09 20:15:56 +0200

    Bugs in syntax repaired

--- cal_project/urls.py
+++ cal_project/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/'), include("advent_calendar/urls.py")
+    path("", include("advent_calendar.urls")),
 ]
