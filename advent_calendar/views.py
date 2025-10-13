57405d87c3e89fd73055f1d408e8a876ee265f02
artur <filutekartur@interia.pl>
2025-10-13 20:33:28 +0200

    views: login required for calendar view

--- advent_calendar/views.py
+++ advent_calendar/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from .models import day
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
 def index(request):
     return render(request,"advent_calendar/index.html")
+@login_required
 def calendar(request):
-    days = day.objects.order_by('day')
-    context = {'days': days}
-    return render(request,"advent_calendar/calendar.html",context)
+        days = day.objects.order_by('day')
+        context = {'days': days}
+        return render(request,"advent_calendar/calendar.html",context)
