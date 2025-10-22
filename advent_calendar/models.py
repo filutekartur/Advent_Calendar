01cd24e1920a77388f61499c0447ea8c3ad020a6
Pawel S <pawsi@sokolow.pl>
2025-10-22 11:56:40 +0200

    days rebuild to server 24 options

--- advent_calendar/models.py
+++ advent_calendar/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class day(models.Model):
-    days = [(1,1),(2,2)]#range(1,25)
+    days = [(x,x) for x in (range(1,25))]
     day = models.SmallIntegerField(blank=False,choices=days,unique=True)
     is_unlocked = models.BooleanField(blank=False)
     award_quote = models.CharField(blank=True)
