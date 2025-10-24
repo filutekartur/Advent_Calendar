72118d83ff6262a08a4cc2c4222412e84e8093ab
artur <filutekartur@interia.pl>
2025-10-24 19:59:18 +0200

    added url with d variable coming in

--- advent_calendar/urls.py
+++ advent_calendar/urls.py
@@ -6,4 +6,5 @@ app_name = "adv_calendar"
 urlpatterns = [
     path("",views.index,name="index"),
     path("calendar/",views.calendar,name="calendar"),
+    path("day/<int:d>",views.day,name="name"),
 ]
