aa726287897f4526ab89143791b2131e60782991
artur <filutekartur@interia.pl>
2025-10-30 19:49:54 +0100

    Day model updated with text field

--- advent_calendar/models.py
+++ advent_calendar/models.py
@@ -9,5 +9,6 @@ class Day(models.Model):
     award_quote = models.CharField(blank=True)
     award_image = models.CharField(blank=True)
     award_link = models.URLField(max_length=255,blank=True)
+    text = models.CharField(blank=False)
     def __str__(self):
         return str(self.day)
