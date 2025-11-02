4303f8c18c20cebea33fed99e9d94fd5c564e7ed
artur <filutekartur@interia.pl>
2025-11-02 20:39:55 +0100

    slice_to_dot tag created

--- advent_calendar/templatetags/adv_cal_tags.py
+++ advent_calendar/templatetags/adv_cal_tags.py
@@ -3,12 +3,6 @@ from django.template.defaultfilters import stringfilter
 
 register = template.Library()
 
-
-def slice_to_sign(value,arg):
-    if isinstance(str,value):
-        l=value.split(arg)
-        return l
-    
 @register.filter
 @stringfilter    
 def slice_to_dot_lr(value,arg):
