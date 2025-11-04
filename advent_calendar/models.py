5745ff164e208a3a12971897b1237ecab5bf0849
artur <filutekartur@interia.pl>
2025-11-04 20:13:01 +0100

    open_date field added to store date of opening reward

--- advent_calendar/models.py
+++ advent_calendar/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Day(models.Model):
     days = [(x,x) for x in (range(1,25))]
     day = models.SmallIntegerField(blank=False,choices=days,unique=True)
+    open_date = models.DateTimeField(blank=True,null=True)
     award_quote = models.CharField(blank=True)
     award_image = models.CharField(blank=True)
     award_link = models.URLField(max_length=255,blank=True)
